a0df99198f9eb3c5c56b53cdd67631366ddc5388
iamraihan <trueraihan@gmail.com>
2022-02-15 19:47:03 +0600

    case added

--- js/app.js
+++ js/app.js
@@ -1,26 +1,35 @@
-// // event listener
+// // phone plus event listener
 document.getElementById('phone-plus').addEventListener('click', function () {
     /* let phoneInput = document.getElementById('phone-input')
     let phoneAmount = document.getElementById('phone-amount')
     let output = parseFloat(phoneInput.value) + 1
     phoneAmount.innerText = output * 1219
     phoneInput.value = output */
-    updateNumber(true)
+    updateNumber('phone', 1219, true)
 
 })
-
+// phone minus event listenr.
 document.getElementById('phone-minus').addEventListener('click', function () {
     /*  let phoneInput = document.getElementById('phone-input')
      let phoneAmount = document.getElementById('phone-amount')
      let output = parseFloat(phoneInput.value) - 1
      phoneAmount.innerText = output * 1219
      phoneInput.value = output */
-    updateNumber(false)
+    updateNumber('phone', 1219, false)
+})
+//case plus eventlistener
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateNumber('case', 59, true)
 })
 
-function updateNumber(isIncrease) {
-    let phoneInput = document.getElementById('phone-input')
-    let phoneAmount = document.getElementById('phone-amount')
+// case minius event listener
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateNumber('case', 59, false)
+})
+
+function updateNumber(product, price, isIncrease) {
+    let phoneInput = document.getElementById(product + '-input')
+    let phoneAmount = document.getElementById(product + '-amount')
     let output = phoneInput.value
     if (isIncrease == true) {
         output = parseFloat(phoneInput.value) + 1
@@ -28,7 +37,7 @@ function updateNumber(isIncrease) {
         output = parseFloat(phoneInput.value) - 1
     }
 
-    phoneAmount.innerText = output * 1219
+    phoneAmount.innerText = output * price
     phoneInput.value = output
 
 }
